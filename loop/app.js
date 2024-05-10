c027bd71c098f0de870e52663233a20b62d2935c
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-05-10 19:00:40 +0500

    still
    continue

---
+++ loop/app.js
@@ -0,0 +1,35 @@
+// question on 1
+// for(var a = 1; a<=10; a++){
+//     console.log(a);
+// }
+
+// question on 2
+// for(var b = 2; b<=20; b++){
+//     if(b%2 === 0){
+//         console.log(b);
+//     }
+// }
+
+
+// question on 3
+// for(var b = 2; b<=20; b++){
+//     if(b%2 === 0){
+//         console.log(b,"even number");
+//     }else{
+//         console.log(b,"odd number");
+//     }
+// }
+
+
+// question on 4
+function factorial(n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+
+
+// question on 5
