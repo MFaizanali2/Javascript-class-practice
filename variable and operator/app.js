6ebc5cae985ae79c4fa4526a0ac69fc59d4841aa
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-05-13 16:25:19 +0500

    operator
    continue

--- variable and operator/app.js
+++ variable and operator/app.js
@@ -1,3 +1,12 @@
-name = "Ali";
-let name = "Faizan";
-console.log(name);
+// var name = "Faizan";
+// var name = "Ali";
+// console.log(name);
+
+// var a = 20;
+// var b = 30;
+// var c = a / b;
+// console.log(c);
+
+var a = 40;
+var b = 30;
+console.log(a == b);
