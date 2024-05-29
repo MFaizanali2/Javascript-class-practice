727b80b9243c13f3fb1e919d9201b4526c7ac0ad
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-05-30 00:02:04 +0500

    ljs line 18
    continue

--- modal/app.js
+++ modal/app.js
@@ -1,8 +1,19 @@
 let show = document.querySelector("#box");
+let isshow = false;
 function showbox(){
-    box.style.opacity = "1"
-    box.style.backgroundColor = "black"
+    // box.style.opacity = "1";
+    // box.style.backgroundColor = "black";
+    // box.style.transition = ".5s";
+    isshow = !isshow;
+    if(isshow){
+        box.style.backgroundColor = "black";
+        box.style.transition = ".5s";
+    }else{
+        box.style.backgroundColor = "red";
+        box.style.transition = ".5s";
+    }
 }
 function hidebox(){
-    box.style.opacity = "0"
+    box.style.opacity = "0";
+    box.style.transition = "1s";
 }
