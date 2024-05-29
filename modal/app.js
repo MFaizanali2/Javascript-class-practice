49125a815b367e06b5239115dcb4a4a71781c27e
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-05-29 23:39:56 +0500

    js line 11
    continue

---
+++ modal/app.js
@@ -0,0 +1,11 @@
+let show = document.querySelector("#box");
+function hidebox(){
+   box.style.backgroundColor = "black";
+   box.style.opacity = "0";
+   box.style.transition = ".2s"
+}
+function showbox(){
+    box.style.backgroundColor = "black";
+    box.style.opacity = "1";
+    box.style.transition = ".2s"
+ }
